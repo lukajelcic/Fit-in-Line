35c0d9a4730b79ca3bac6a02f0b657c4b0065fa6
Fit-in-Linec <lukajelcic23@gmail.com>
2020-03-19 21:52:03 +0100

    create components for slider

--- src/components/QuickLinksSlider/QuickLinks.js
+++ src/components/QuickLinksSlider/QuickLinks.js
@@ -1,82 +1,45 @@
 import React from 'react';
-import RegButton from '../Buttons/RegButton'
 
 import './quickLinks.css'
+import SliderBox from './SliderBox';
 
 class QuickLinks extends React.Component {
     render() {
         return (
             <div className="linksSlider">
+
                 <div className="sliderChild">
                     <div className="imagecon">
-                        <div className="slider-wrap">
-                            <div className="slider-box">
-                                <h1 className="slider-title">
-                                    Prikljuci se Fit in Line timu
-                            </h1>
-                                <p className="slider-txt">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                            </p>
-                                <RegButton name="Prijavi se" />
-                            </div>
-                        </div>
+                        <SliderBox
+                            title="Marko"
+                            content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                        />
+                    </div>
+
+                    <div className="imagecon">
+                        <SliderBox
+                            title="Una"
+                            content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                        />
+                    </div>
+
+                    <div className="imagecon">
+                        <SliderBox
+                            title="Luka"
+                            content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                        />
                     </div>
                     <div className="imagecon">
-                        <div className="slider-wrap">
-                            <div className="slider-box">
-                                <h1 className="slider-title">
-                                    Prikljuci se Fit in Line timu
-                            </h1>
-                                <p className="slider-txt">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                            </p>
-                            <RegButton name="Prijavi se" />
-                            </div>
-                        </div>
+                        <SliderBox
+                            title="Janko"
+                            content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                        />
                     </div>
                     <div className="imagecon">
-                        <div className="slider-wrap">
-                            <div className="slider-box">
-                                <h1 className="slider-title">
-                                    Prikljuci se Fit in Line timu
-                            </h1>
-                                <p className="slider-txt">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                            </p>
-                            <RegButton name="Prijavi se" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="imagecon">
-                        <div className="slider-wrap">
-                            <div className="slider-box">
-                                <h1 className="slider-title">
-                                    Prikljuci se Fit in Line timu
-                            </h1>
-                                <p className="slider-txt">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                            </p>
-                            <RegButton name="Prijavi se" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="imagecon">
-                        <div className="slider-wrap">
-                            <div className="slider-box">
-                                <h1 className="slider-title">
-                                    Prikljuci se Fit in Line timu
-                            </h1>
-                                <p className="slider-txt">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                            </p>
-                            <RegButton name="Prijavi se" />
-                            </div>
-                        </div>
+                        <SliderBox
+                            title="Fit"
+                            content="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                        />
                     </div>
                 </div>
 
---
+++ src/components/QuickLinksSlider/SliderBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RegButton from '../Buttons/RegButton';
+
+function SliderBox(props) {
+    return (
+        <div className="slider-wrap">
+            <div className="slider-box">
+                <h1 className="slider-title">{props.title}</h1>
+                <p className="slider-txt">{props.content}</p>
+                <RegButton name="Prijavi se" />
+            </div>
+        </div>
+    )
+}
+
+export default SliderBox;
